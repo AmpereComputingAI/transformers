fa47607699130c8e74952760e372533f4f931ea0
Karol Kontny <karol@amperecomputing.com>
2024-01-26 14:49:56 +0100

    Add logs

--- src/transformers/models/mixtral/modeling_mixtral.py
+++ src/transformers/models/mixtral/modeling_mixtral.py
@@ -728,7 +728,11 @@ class MixtralSparseMoeBlock(nn.Module):
 
         # One hot encode the selected experts to create an expert mask
         # this will be used to easily index which expert is going to be sollicitated
+        print(selected_experts)
         expert_mask = torch.nn.functional.one_hot(selected_experts, num_classes=self.num_experts).permute(2, 1, 0)
+        print(expert_mask)
+        expert_mask = expert_mask.permute(2, 1, 0)
+        print(expert_mask)
 
         # Loop over all available experts in the model and perform the computation on each expert
         for expert_idx in range(self.num_experts):
